eec56a71bdd070a5242d492fd2f63eb83cda53f2
Esteban Arias <estebanariaslima@gmail.com>
2021-12-16 17:53:58 -0400

    Se Agregan nuevas excusas

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 window.onload = function() {
-    let myExcuseArray = [" el perro se comio mi tarea"," me quede dormido, no sono la alarma"," se fue el internet y no pude entrar a clase"," no tengo ganas", " no me presiones"]
+    let myExcuseArray = [" el perro se comio mi tarea",
+    " me quede dormido, no sono la alarma",
+    " se fue el internet y no pude entrar a clase",
+    " no tengo ganas", 
+    " no me presiones",
+    " Ermesto Nedima me dijo que no lo hiciera",
+    " un saludo para el admin del grupo",
+    " solo tenias que seguir el maldito tren CJ",
+    " missingNo",
+    " I am Iron Man",
+    " Requiescat in pace",
+    " JavaScript",
+    " 4Geeks Academy",
+    " En la litera yo duermo abajo y arriba españa",
+    " Su excusa no se esta cargando..."]
 
     function getRandomElement() {
         return myExcuseArray [Math.floor(Math.random() * myExcuseArray.length)];
     }
 
-    document.getElementById("excusa").innerHTML = getRandomElement()
+    document.querySelector("#excusa").innerHTML = getRandomElement()
 }
